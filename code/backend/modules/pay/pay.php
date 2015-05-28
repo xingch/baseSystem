326037716d0aecbe443c872ec4ece972babeab2d
wanggang <wangg@91160.com>
2015-05-28 18:04:06 +0800

    create modules
    create modules

---
+++ code/backend/modules/pay/pay.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace backend\modules\pay;
+
+class pay extends \yii\base\Module
+{
+    public $controllerNamespace = 'backend\modules\pay\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
